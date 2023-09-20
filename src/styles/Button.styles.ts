70a1a6b7948c1735631a483e70f00965d102a006
jaroslaw-sasiadek <jaroslawsasiadek@gmail.com>
2023-09-20 23:11:51 +0200

    Add styles

---
+++ src/styles/Button.styles.ts
@@ -0,0 +1,14 @@
+export const ButtonStyle = {
+	green: {
+		enable:
+			"px-[24px] bg-[--c3] hover:scale-110 transition h-[42px] rounded-[2px] text-[14px] text-[--c5] cursor-pointer",
+		disable:
+			"px-[24px] bg-[--c1] border-[1px] text-[--c2] border-[--c2] transitionh-[42px] rounded-[2px] text-[14px] text-[--c5]",
+	},
+	text: {
+		enable:
+			"flex px-[24px] hover:text-[--c3] transition h-[42px] text-[14px] text-black font-[600] cursor-pointer",
+		disable:
+			"flex px-[24px] border-[1px] text-[--c2] border-[--c2] transition w-[134px] h-[42px] text-[14px] text-[--c5]",
+	},
+};
---
+++ src/styles/TextField.styles.ts
@@ -0,0 +1,7 @@
+export const TextFieldStyle = {
+	footer:
+		"focus:outline-none w-[420px] h-[42px] px-[10px] bg-[--c2] rounded-[2px] text-[18px] font-[400] text-[--c1]",
+	other:
+		"focus:outline-none h-[42px] px-[10px] bg-[--c5] rounded-[2px] text-[18px] font-[400] text-[--c1]",
+	area: "focus:outline-none px-[10px] bg-[--c5] rounded-[2px] text-[18px] font-[400] text-[--c1]",
+};
