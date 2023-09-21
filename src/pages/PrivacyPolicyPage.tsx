164a00f12fe6d994431f9c9a55ac105be93d96f5
jaroslaw-sasiadek <jaroslawsasiadek@gmail.com>
2023-09-21 19:29:28 +0200

    Add mock-up pages

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -2,8 +2,42 @@ import { Content } from "../components/main/Content";
 
 export const PrivacyPolicyPage = () => {
 	return (
-		<Content title="EcoBazarek | Polityka prywatności">
-			<h1 className="text-[48px] font-[900] text-[--c1]">PrivacyPolicyPage</h1>
+		<Content
+			title="EcoBazarek | Polityka prywatności"
+			keywords="polityka, prywatności"
+			description="Zapoaznaj się z polityką prywatności naszej aplikacji"
+			isLoading={false}
+		>
+			<h1 className="py-[40px] text-[48px] font-[900] text-[--c1] uppercase">
+				Polityka prywatności
+			</h1>
+			<p className="text-[16px] text-[--c1]">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+				<br />
+				<br />
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+				<br />
+				<br />
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+			</p>
 		</Content>
 	);
 };
--- src/pages/TermsOfServicePage.tsx
+++ src/pages/TermsOfServicePage.tsx
@@ -2,8 +2,42 @@ import { Content } from "../components/main/Content";
 
 export const TermsOfServicePage = () => {
 	return (
-		<Content title="EcoBazarek | Warunki użytkowania">
-			<h1 className="text-[48px] font-[900] text-[--c1]">TermsOfServicePage</h1>
+		<Content
+			title="EcoBazarek | Warunki użytkowania"
+			keywords="warunki, użytkowania"
+			description="Zapoznaj się z warunkami użytkowania aplikacji"
+			isLoading={false}
+		>
+			<h1 className="py-[40px] text-[48px] font-[900] text-[--c1] uppercase">
+				Warunki użytkowania
+			</h1>
+			<p className="text-[16px] text-[--c1]">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+				<br />
+				<br />
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+				<br />
+				<br />
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
+				reiciendis autem ad esse laudantium? Dolorem debitis voluptatum delectus
+				omnis enim, aliquid aliquam eos pariatur quos dolor! Optio ipsum
+				architecto quae. Lorem ipsum, dolor sit amet consectetur adipisicing
+				elit. Dolores asperiores ad placeat consequuntur et iusto aliquid
+				voluptate labore exercitationem. Est, dicta sequi. Est iure animi
+				incidunt temporibus repellat facilis suscipit.
+			</p>
 		</Content>
 	);
 };
