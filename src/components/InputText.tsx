62b613530ddf14ad45d752ed9a5d27ee10b3ffbc
jaroslaw-sasiadek <jaroslawsasiadek@gmail.com>
2023-09-21 16:47:44 +0200

    Simplify InputText & RegistrationPage

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -2,20 +2,21 @@ import { TextFieldStyle } from "../styles/TextField.styles";
 
 export interface InputTextProps
 	extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "className"> {
+	divClass?: string;
 	spanName: string;
 	inputClass?: string;
 }
 
 export const InputText = (props: InputTextProps) => {
-	const { spanName, inputClass, ...other } = props;
+	const { divClass, spanName, inputClass, ...other } = props;
 
 	return (
-		<>
+		<div className={divClass || "w-full"}>
 			<span className="text-[12px] font-[500]">{spanName}</span>
 			<input
 				{...other}
 				className={`mb-[34px] w-full ${TextFieldStyle.other} ${inputClass}`}
 			/>
-		</>
+		</div>
 	);
 };
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -4,11 +4,32 @@ import { UserCreateProps } from "../interfaces/UserProps";
 import { UserContext } from "../context/UserContext";
 import { Data } from "../API/_Data";
 import { Content } from "../components/main/Content";
-import { InputText } from "../components/InputText";
+import { InputText, InputTextProps } from "../components/InputText";
 import { TextFieldStyle } from "../styles/TextField.styles";
 import { ButtonStyle } from "../styles/Button.styles";
 
+const defaultUserState = (): UserCreateProps => ({
+	firstName: "",
+	lastName: "",
+	email: "",
+	phone: "",
+	password: "",
+	farmName: "",
+	farmDesc: "",
+	street: "",
+	streetNumber: "",
+	flatNumber: "",
+	city: "",
+	postCode: "",
+	country: "",
+	voivodeship: "",
+	county: "",
+	district: "",
+	repeatPassword: "",
+});
+
 export const RegistrationPage = () => {
+	const [formData, setFormData] = useState<UserCreateProps>(defaultUserState);
 	const { isLoggedIn } = useContext(UserContext);
 	const [isDisabled, setIsDisabled] = useState(false);
 	const [buttonStyle, setButtonStyle] = useState(ButtonStyle.green.enable);
@@ -17,14 +38,27 @@ export const RegistrationPage = () => {
 	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
 		setIsDisabled(true);
 		setButtonStyle(ButtonStyle.green.disable);
-		const formData = new FormData(event.target as HTMLFormElement);
-		const data = Object.fromEntries(formData) as unknown as UserCreateProps;
-		const response = await Data.Users.PostUsers(event, data);
+		// const formData = new FormData(event.target as HTMLFormElement);
+		// const data = Object.fromEntries(formData) as unknown as UserCreateProps;
+		// const response = await Data.Users.PostUsers(event, data);
+		const response = await Data.Users.PostUsers(event, formData);
 		if (response && response !== "error") setErrors(response);
 		if (!response) return;
 		setIsDisabled(false);
 		setButtonStyle(ButtonStyle.green.enable);
 	}
+
+	const getFieldProps = (
+		key: keyof UserCreateProps
+	): Omit<InputTextProps, "divClass" | "spanName"> => ({
+		name: key,
+		value: formData[key],
+		onChange: (event) =>
+			setFormData((previous) => ({
+				...previous,
+				[key]: event.target.value,
+			})),
+	});
 
 	const UlErrors = () => {
 		return (
@@ -61,75 +95,61 @@ export const RegistrationPage = () => {
 					</h1>
 					<form onSubmit={handleSubmit}>
 						<div className="flex w-full gap-[15px]">
-							<div className="w-full">
-								<InputText
-									spanName="Imię*"
-									name="firstName"
-									type="text"
-									autoComplete="given-name"
-									required
-								/>
-							</div>
-							<div className="w-full">
-								<InputText
-									spanName="Nazwisko*"
-									name="lastName"
-									type="text"
-									autoComplete="family-name"
-									required
-								/>
-							</div>
+							<InputText
+								spanName="Imię*"
+								{...getFieldProps("firstName")}
+								type="text"
+								autoComplete="given-name"
+								required
+							/>
+							<InputText
+								spanName="Nazwisko*"
+								{...getFieldProps("lastName")}
+								type="text"
+								autoComplete="family-name"
+								required
+							/>
 						</div>
 						<div className="flex w-full gap-[15px]">
-							<div className="w-full">
-								<InputText
-									spanName="Email*"
-									name="email"
-									type="email"
-									autoComplete="email"
-									required
-								/>
-							</div>
-							<div className="w-full">
-								<InputText
-									spanName="Telefon*"
-									name="phone"
-									type="phone"
-									autoComplete="phone"
-									required
-								/>
-							</div>
+							<InputText
+								spanName="Email*"
+								{...getFieldProps("email")}
+								type="email"
+								autoComplete="email"
+								required
+							/>
+							<InputText
+								spanName="Telefon*"
+								{...getFieldProps("phone")}
+								type="phone"
+								autoComplete="phone"
+								required
+							/>
 						</div>
 						<div className="flex w-full gap-[15px]">
-							<div className="w-full">
-								<InputText
-									spanName="Hasło*"
-									name="password"
-									type="password"
-									required
-								/>
-							</div>
-							<div className="w-full">
-								<InputText
-									spanName="Powtórz hasło*"
-									name="repeatPassword"
-									type="password"
-									required
-								/>
-							</div>
+							<InputText
+								spanName="Hasło*"
+								{...getFieldProps("password")}
+								type="password"
+								required
+							/>
+							<InputText
+								spanName="Powtórz hasło*"
+								{...getFieldProps("repeatPassword")}
+								type="password"
+								required
+							/>
 						</div>
 						<UlErrors />
 						<h2 className="py-[35px] text-[14px] font-[900] text-[--c1] uppercase">
 							Adres i informacje o gospodarstwie
 						</h2>
-						<div className="w-full">
-							<InputText
-								spanName="Nazwa gospodarstwa*"
-								name="farmName"
-								type="text"
-								required
-							/>
-						</div>
+						<InputText
+							spanName="Nazwa gospodarstwa*"
+							{...getFieldProps("farmName")}
+							type="text"
+							required
+						/>
 						<span className="text-[12px] font-[500]">Opis gospodarstwa</span>
 						<textarea
 							className={`mb-[34px] w-full block ${TextFieldStyle.area} h-[130px]`}
@@ -139,82 +159,66 @@ export const RegistrationPage = () => {
 							<div className="w-[50%]">
 								<InputText
 									spanName="Ulica*"
-									name="street"
+									{...getFieldProps("street")}
 									type="text"
 									required
 								/>
 							</div>
 							<div className="flex w-[50%] gap-[16px]">
-								<div>
-									<InputText
-										spanName="Numer domu*"
-										name="streetNumber"
-										type="text"
-										required
-									/>
-								</div>
-								<div>
-									<InputText
-										spanName="Numer mieszkania"
-										name="flatNumber"
-										type="text"
-									/>
-								</div>
-							</div>
-						</div>
-						<div className="flex gap-[16px]">
-							<div className="w-full">
 								<InputText
-									spanName="Miasto/ Wieś*"
-									name="city"
+									spanName="Numer domu*"
+									{...getFieldProps("streetNumber")}
 									type="text"
 									required
 								/>
-							</div>
-							<div className="w-full">
 								<InputText
-									spanName="Kod pocztowy*"
-									name="postCode"
+									spanName="Numer mieszkania"
+									{...getFieldProps("flatNumber")}
 									type="text"
-									required
 								/>
 							</div>
 						</div>
 						<div className="flex gap-[16px]">
-							<div className="w-full">
-								<InputText
-									spanName="Województwo*"
-									name="voivodeship"
-									type="text"
-									required
-								/>
-							</div>
-							<div className="w-full">
-								<InputText
-									spanName="Powiat*"
-									name="county"
-									type="text"
-									required
-								/>
-							</div>
+							<InputText
+								spanName="Miasto/ Wieś*"
+								{...getFieldProps("city")}
+								type="text"
+								required
+							/>
+							<InputText
+								spanName="Kod pocztowy*"
+								{...getFieldProps("postCode")}
+								type="text"
+								required
+							/>
 						</div>
 						<div className="flex gap-[16px]">
-							<div className="w-full">
-								<InputText
-									spanName="Gmina*"
-									name="district"
-									type="text"
-									required
-								/>
-							</div>
-							<div className="w-full">
-								<InputText
-									spanName="Kraj*"
-									name="country"
-									type="text"
-									required
-								/>
-							</div>
+							<InputText
+								spanName="Województwo*"
+								{...getFieldProps("voivodeship")}
+								type="text"
+								required
+							/>
+							<InputText
+								spanName="Powiat*"
+								{...getFieldProps("county")}
+								type="text"
+								required
+							/>
+						</div>
+						<div className="flex gap-[16px]">
+							<InputText
+								spanName="Gmina*"
+								{...getFieldProps("district")}
+								type="text"
+								required
+							/>
+							<InputText
+								spanName="Kraj*"
+								{...getFieldProps("country")}
+								type="text"
+								required
+							/>
 						</div>
 						<div className="mt-[30px] flex w-full justify-end">
 							<input
