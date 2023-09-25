0d91f5ee90a01fbe5da4a9e58db1613656a0d2b4
jaroslaw-sasiadek <jaroslawsasiadek@gmail.com>
2023-09-25 11:46:32 +0200

    Update code

--- src/pages/ProfilePage/components/PasswordForm/getFieldProps.ts
+++ src/pages/ProfilePage/components/PasswordForm/getFieldProps.ts
@@ -1,30 +1,34 @@
 import { ResponseErrors } from "../../../../interfaces";
 
-export const getFieldProps = <T>(
-	keys: (keyof T)[],
-	data: T,
-	setFormData: React.Dispatch<React.SetStateAction<T>>,
-	setErrors: React.Dispatch<React.SetStateAction<ResponseErrors>>
-) => {
-	const fieldPropsMap: Record<string, unknown> = {};
-
-	keys.forEach((key) => {
-		fieldPropsMap[key as string] = {
-			name: key as string,
-			value: data[key],
-			onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-				setErrors((previous) => {
-					const current = { ...previous };
-					delete current[key as string];
-					return current;
-				});
-				setFormData((previous) => ({
-					...previous,
-					[key]: event.target.value,
-				}));
-			},
-		};
-	});
-
-	return fieldPropsMap;
+type InputsProps<K extends keyof T, T> = {
+	name: K;
+	value: T[K];
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
+export const getFieldProps = <T, K extends keyof T>(
+	key: K,
+	{
+		data,
+		setData,
+		setErrors,
+	}: {
+		data: T;
+		setData: React.Dispatch<React.SetStateAction<T>>;
+		setErrors: React.Dispatch<React.SetStateAction<ResponseErrors>>;
+	}
+): InputsProps<K, T> => ({
+	name: key,
+	value: data[key],
+	onChange: (event) => {
+		setErrors((previous) => {
+			const current = { ...previous };
+			delete current[key as string];
+			return current;
+		});
+		setData((previous) => ({
+			...previous,
+			[key]: event.target.value,
+		}));
+	},
+});
